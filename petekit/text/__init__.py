6a3362e6d8a950bdc99540f8e339e3dd4a30a002
Jörg P. Schäfer <1178078+frygge@users.noreply.github.com>
2026-09-17 17:07:11 +0200

    text: rename web_navigator.py -> web_scraper.py, update imports

--- petekit/text/__init__.py
+++ petekit/text/__init__.py
@@ -1,6 +1,6 @@
 from petekit.text.buffer_manager import BufferManager
 from petekit.text.text_editor import TextEditor
-from petekit.text.web_navigator import WebScraper
+from petekit.text.web_scraper import WebScraper
 
 __all__ = [
     "BufferManager",
