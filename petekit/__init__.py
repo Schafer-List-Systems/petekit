bdf7d2dda5dae00b86bd1c9b16e8be0ecde18d69
Jörg P. Schäfer <1178078+frygge@users.noreply.github.com>
2026-09-19 05:22:10 +0200

    fix __init__ imports to match only tracked files, add StreamProcessor export

--- petekit/__init__.py
+++ petekit/__init__.py
@@ -1,12 +1,10 @@
 from petekit.text import TextEditor, WebScraper
 from petekit.stream import (
     StreamBufferManager,
-    StreamObserver,
-    StreamForwarder,
-    RegexStreamObserver,
     Basher,
     Connector,
     SandboxedBasher,
+    StreamProcessor,
 )
 from petekit.image import (
     ImageBufferManager,
@@ -21,12 +19,10 @@ __all__ = [
     "TextEditor",
     "WebScraper",
     "StreamBufferManager",
-    "StreamObserver",
-    "StreamForwarder",
-    "RegexStreamObserver",
     "Basher",
     "Connector",
     "SandboxedBasher",
+    "StreamProcessor",
     "ImageBufferManager",
     "CameraObserver",
     "Screenshooter",
--- petekit/stream/__init__.py
+++ petekit/stream/__init__.py
@@ -1,22 +1,15 @@
-from petekit.stream.stream_buffer_manager import StreamBufferManager, StreamBufferRules, Rule
-from petekit.stream.stream_observer import StreamObserver
-from petekit.stream.stream_forwarder import StreamForwarder
-from petekit.stream.pattern_matchers.regex_stream import RegexStreamObserver, RegexPattern
+from petekit.stream.stream_buffer_manager import StreamBufferManager
 from petekit.stream.basher import Basher, BashHandle
 from petekit.stream.connector import Connector, ConnectionHandle
 from petekit.stream.sandboxed_basher import SandboxedBasher
+from petekit.stream.stream_processor import StreamProcessor
 
 __all__ = [
     "StreamBufferManager",
-    "StreamBufferRules",
-    "Rule",
-    "StreamObserver",
-    "StreamForwarder",
-    "RegexStreamObserver",
-    "RegexPattern",
     "Basher",
     "BashHandle",
     "Connector",
     "ConnectionHandle",
     "SandboxedBasher",
+    "StreamProcessor",
 ]
